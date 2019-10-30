34a56ef50c04f79eb612eca92e6c5667525e34ef
Zexin Lin <linzexin1214@gmail.com>
2019-10-31 02:25:51 +1100

    added test for exit

--- src/unsw/dungeonTest/ExitTest.java
+++ src/unsw/dungeonTest/ExitTest.java
@@ -2,13 +2,28 @@ package unsw.dungeonTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ExitTest {
+import unsw.dungeon.*;
 
+class ExitTest {
+	
+	Dungeon dungeon;
+	Entity exit;
+	
+	@BeforeEach
+	void init() {
+		dungeon = new Dungeon(5, 5);
+		exit = new Exit(4, 4);
+		dungeon.addEntity(exit);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void ExitCanBeStepOn() {
+		assertTrue(exit.getX() == 4);
+		assertTrue(exit.getY() == 4);
+		assertTrue(dungeon.canGoThere(4, 4));
 	}
 
 }
