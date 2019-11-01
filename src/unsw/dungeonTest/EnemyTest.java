f365b6fdce2cb8bf6022d5af3b1aeccab340ce01
Zexin Lin <linzexin1214@gmail.com>
2019-11-01 18:53:00 +1100

    added test cases for enemy test

--- src/unsw/dungeonTest/EnemyTest.java
+++ src/unsw/dungeonTest/EnemyTest.java
@@ -2,13 +2,46 @@ package unsw.dungeonTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class EnemyTest {
+import unsw.dungeon.*;
 
+class EnemyTest {
+	
+	Dungeon dungeon;
+	Player player;
+	Enemy enemy;
+	
+	@BeforeEach
+	void init() {
+		dungeon = new Dungeon(5, 5);
+		Player player = new Player(dungeon, 0, 0);
+		dungeon.addEntity(player);
+		dungeon.setPlayer(player);
+		Enemy enemy = new Enemy(dungeon, 4, 4);
+		dungeon.addEntity(enemy);
+		dungeon.setEnemyObserver();
+	}
+	
 	@Test
-	void test() {
+	void ApproachPlayerOneSqrPerMove() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	void ApproachPlayerOneSqrPerMove() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void ApproachPlayerOneSqrPerMove() {
+		fail("Not yet implemented");
+	}@Test
+	
+	void ApproachPlayerOneSqrPerMove() {
+		fail("Not yet implemented");
+	}
+
 
 }
