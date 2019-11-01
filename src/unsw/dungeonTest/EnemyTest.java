1798d20e2a8494b5d443dc330d3868e249aac19f
Zexin Lin <linzexin1214@gmail.com>
2019-11-02 01:32:49 +1100

    finished tests for enemy

--- src/unsw/dungeonTest/EnemyTest.java
+++ src/unsw/dungeonTest/EnemyTest.java
@@ -16,31 +16,89 @@ class EnemyTest {
 	@BeforeEach
 	void init() {
 		dungeon = new Dungeon(5, 5);
-		Player player = new Player(dungeon, 0, 0);
+		player = new Player(dungeon, 0, 0);
 		dungeon.addEntity(player);
 		dungeon.setPlayer(player);
-		Enemy enemy = new Enemy(dungeon, 4, 4);
+		enemy = new Enemy(dungeon, 4, 4);
 		dungeon.addEntity(enemy);
 		dungeon.setEnemyObserver();
 	}
 	
 	@Test
+	void EnemyCannotGoThroughWall() {
+		Wall wall = new Wall(1, 1);
+		dungeon.addEntity(wall);
+		assertTrue(!dungeon.canGoThere(1, 1));
+		System.out.println("EnemyCannotGoThroughWall passed");
+	}
+	
+	@Test
 	void ApproachPlayerOneSqrPerMove() {
-		fail("Not yet implemented");
+		player.moveRight();
+		assertTrue(player.getX() == 1 && player.getY() == 0);
+		player.notifyEnemy();
+		assertTrue(enemy.getX() == 3 || enemy.getX() == 5 || enemy.getY() == 3 || enemy.getY() == 5);
+		System.out.println("ApproachPlayerOneSqrPerMove passed");
 	}
 	
 	@Test
 	void KillPlayerWithoutWeapon() {
-		fail("Not yet implemented");
+		player.moveDown();
+		player.notifyEnemy();
+		player.moveRight();
+		player.notifyEnemy();
+		player.moveDown();
+		player.notifyEnemy();
+		player.moveDown();
+		player.notifyEnemy();
+		assertTrue(player.battleEnemy() == false);
+		assertTrue(!player.checkAlive());
+		System.out.println("KillPlayerWithoutWeapon passed");
+		//System.out.println(enemy.getX() +  "," + enemy.getY());
 	}
 	
 	@Test
 	void KilledByPlayerWithSword() {
-		fail("Not yet implemented");
-	}@Test
+		Sword sword = new Sword(1, 1);
+		dungeon.addEntity(sword);
+		player.moveDown();
+		player.notifyEnemy();
+		player.moveRight();
+		player.notifyEnemy();
+		player.pickUp();
+		assertTrue(player.hasSword());
+		player.moveDown();
+		player.notifyEnemy();
+		player.moveDown();
+		player.notifyEnemy();
+		assertTrue(player.battleEnemy());
+		assertTrue(player.checkAlive());
+		assertTrue(!dungeon.hasEnemy());
+		System.out.println("KilledByPlayerWithSword passed");
+	}
 	
-	void KilledByPlayerWithPotionp() {
-		fail("Not yet implemented");
+	@Test
+	void KilledByPlayerWithPotion() {
+		Invincibility potion = new Invincibility(1, 1);
+		dungeon.addEntity(potion);
+		player.moveDown();
+		player.notifyEnemy();
+		player.moveRight();
+		player.notifyEnemy();
+		player.pickUp();
+		assertTrue(player.hasInvincibility());
+		player.useInvincibility();
+		player.moveDown();
+		player.moveDown();
+		player.moveDown();
+		player.moveRight();
+		player.notifyEnemy();
+		player.moveRight();
+		assertTrue(player.hasInvincibility());
+		assertTrue(player.battleEnemy());
+		assertTrue(player.checkAlive());
+		assertTrue(!dungeon.hasEnemy());
+		System.out.println("KilledByPlayerWithPotion passed");
 	}
 
 
