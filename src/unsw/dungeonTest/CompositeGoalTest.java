0cd6afd59f53eea93a3c50e263893422b8a60ad2
Zexin Lin <linzexin1214@gmail.com>
2019-11-02 21:04:05 +1100

    finished tests for composite goals, tests for epic story4 done

--- src/unsw/dungeonTest/CompositeGoalTest.java
+++ src/unsw/dungeonTest/CompositeGoalTest.java
@@ -23,15 +23,131 @@ class CompositeGoalTest {
 		player = new Player(dungeon, 0, 0);
 		dungeon.setPlayer(player);
 		dungeon.addEntity(player);
-		
+		treasureGoal = new GoalLeaf("treasure", dungeon);
+		boulderGoal = new GoalLeaf("boulders", dungeon);
+		exitGoal = new GoalLeaf("exit", dungeon);
 		enemyGoal = new GoalLeaf("enemies", dungeon);
-		dungeon.addGoal(enemyGoal);
 	}
 	
 	@Test
-	void ShowEnemyGoal() {
-		assertTrue(dungeon.getGameGuide().equals("(kill all enemies)"));
-		System.out.println("ShowEnemyGoal passed");
+	void ShowANDGoals() {
+		goals = new GoalComposite("and", dungeon);
+		goals.add(treasureGoal);
+		goals.add(boulderGoal);
+		goals.add(exitGoal);
+		goals.add(enemyGoal);
+		dungeon.addGoal(goals);
+		assertTrue(dungeon.getGameGuide().equals("[(pick up all the treasure) and\n" + "(turn on all the floor switches) and\n" + "(getting to exit) and\n" + "(kill all enemies)]"));           
+		System.out.println("ShowANDGoals passed");
 	}
-
+	
+	@Test
+	void ShowORGoals() {
+		goals = new GoalComposite("or", dungeon);
+		goals.add(treasureGoal);
+		goals.add(boulderGoal);
+		goals.add(exitGoal);
+		goals.add(enemyGoal);
+		dungeon.addGoal(goals);
+		assertTrue(dungeon.getGameGuide().equals("[(pick up all the treasure) or\n" + "(turn on all the floor switches) or\n" + "(getting to exit) or\n" + "(kill all enemies)]"));           
+		System.out.println("ShowORGoals passed");
+	}
+	
+	@Test
+	void ShowComplexGoals() {
+		goals = new GoalComposite("or", dungeon);
+		GoalComponent goal1 = new GoalComposite("and", dungeon);
+		goal1.add(treasureGoal);
+		goal1.add(exitGoal);
+		GoalComponent goal2 = new GoalComposite("and", dungeon);
+		goal2.add(boulderGoal);
+		goal2.add(enemyGoal);
+		goals.add(goal1);
+		goals.add(goal2);
+		dungeon.addGoal(goals);
+		assertTrue(dungeon.getGameGuide().equals("[[(pick up all the treasure) and\n" + "(getting to exit)] or\n" + "[(turn on all the floor switches) and\n" + "(kill all enemies)]]"));           
+		System.out.println("ShowComplexGoals passed");
+	}
+	
+	@Test
+	void CompleteANDGoals() {
+		goals = new GoalComposite("and", dungeon);
+		goals.add(treasureGoal);
+		goals.add(exitGoal);
+		dungeon.addGoal(goals);
+		Treasure trea = new Treasure(1, 1);
+		Exit exit = new Exit(2, 2);
+		dungeon.addEntity(trea);
+		dungeon.addEntity(exit);
+		player.moveDown();
+		player.moveRight();
+		player.pickUp();
+		assertTrue(!dungeon.checkgoal());
+		player.moveDown();
+		player.moveRight();
+		assertTrue(dungeon.checkgoal());
+		System.out.println("CompleteANDGoals passed");
+	}
+	
+	@Test
+	void CompleteORGoals() {
+		goals = new GoalComposite("or", dungeon);
+		goals.add(treasureGoal);
+		goals.add(exitGoal);
+		dungeon.addGoal(goals);
+		Treasure trea = new Treasure(1, 1);
+		Exit exit = new Exit(2, 2);
+		dungeon.addEntity(trea);
+		dungeon.addEntity(exit);
+		player.moveDown();
+		player.moveRight();
+		player.pickUp();
+		assertTrue(dungeon.checkgoal());
+		System.out.println("CompleteORGoals passed");
+	}
+	
+	@Test
+	void CompleteComplexGoals() {
+		goals = new GoalComposite("or", dungeon);
+		GoalComponent goal1 = new GoalComposite("and", dungeon);
+		goal1.add(treasureGoal);
+		goal1.add(exitGoal);
+		GoalComponent goal2 = new GoalComposite("and", dungeon);
+		goal2.add(boulderGoal);
+		goal2.add(enemyGoal);
+		goals.add(goal1);
+		goals.add(goal2);
+		dungeon.addGoal(goals);
+		Treasure trea = new Treasure(1, 1);
+		Exit exit = new Exit(2, 2);
+		dungeon.addEntity(trea);
+		dungeon.addEntity(exit);
+		player.moveDown();
+		player.moveRight();
+		player.pickUp();
+		player.moveDown();
+		player.moveRight();
+		assertTrue(dungeon.checkgoal());
+		System.out.println("CompleteComplexGoals passed");
+	}
+	
+	@Test
+	void CannotCompleteIfExitFirst() {
+		goals = new GoalComposite("and", dungeon);
+		goals.add(treasureGoal);
+		goals.add(exitGoal);
+		dungeon.addGoal(goals);
+		Treasure trea = new Treasure(2, 2);
+		Exit exit = new Exit(1, 1);
+		dungeon.addEntity(trea);
+		dungeon.addEntity(exit);
+		player.moveDown();
+		player.moveRight();
+		assertTrue(!dungeon.checkgoal());
+		player.moveDown();
+		player.moveRight();
+		player.pickUp();
+		assertTrue(!dungeon.checkgoal());
+		System.out.println("CannotCompleteIfExitFirst passed");
+	}
 }
